58c8bf9fac0ff3fc2e55f273bc123457f2057cae
julianmv05 <94500914+julianmv05@users.noreply.github.com>
2021-11-16 20:00:53 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# github
+# PROC9_P5Juego
---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background("blue");
+text("mi nombre es julian y me gusta la pizza",100,200)
+}
+
+
+
+
